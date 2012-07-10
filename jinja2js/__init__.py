f00c308265eab98846d979184681ea4b495536e3
Matt Basta <me@mattbasta.com>
2012-07-10 16:28:06 -0700

    Added helper for django

--- jinja2js/__init__.py
+++ jinja2js/__init__.py
@@ -1,1 +1,29 @@
 from revrender import extract, extract_template, set_env
+
+
+renderers = []
+
+# Try to use Jingo if we have it.
+try:
+    import jingo
+    renderers.append(jingo.render)
+except ImportError:
+    pass
+
+# Use `django.shortcuts.render` if we don't.
+try:
+    import django.shortcuts as shortcuts
+    renderers.append(shortcuts.render)
+except ImportError:
+    pass
+
+
+def render_or_extract(request, template, context=None, **kwargs):
+    if request.is_ajax():
+        return extract(request, template, context, **kwargs)
+
+    if renderers:
+        return renderers[0](request, template, context, **kwargs)
+
+    raise Exception("No renderers could be found.")
+
